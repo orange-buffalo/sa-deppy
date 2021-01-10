db327fa5a1f787c0714649ba36d1ebfe1fdda435
orange-buffalo <orange-buffalo@users.noreply.github.com>
2021-01-10 21:21:23 +1100

    #1: improvements to frontend dependencies updates check

--- src/npm-updater.js
+++ src/npm-updater.js
@@ -1,6 +1,7 @@
 const ncu = require('npm-check-updates');
 const fs = require('fs').promises;
-const {execSync} = require('child_process');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 function escapeRegex(string) {
   return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
@@ -29,60 +30,69 @@ class NpmUpdater {
 
     const updatesList = [];
 
-    const ncuState = await ncu.run({
-      cwd: workingDirectory,
-      packageFile,
-      upgrade: true,
-    });
+    try {
+      const ncuState = await ncu.run({
+        cwd: workingDirectory,
+        packageFile,
+        upgrade: true,
+      });
 
-    let updatedPackageJson = excludedDependencies.length && (await fs.readFile(packageFile)).toString();
+      let updatedPackageJson = excludedDependencies.length && (await fs.readFile(packageFile)).toString();
 
-    for (let packageName of Object.keys(ncuState)) {
-      const packageVersion = ncuState[packageName];
-      const shouldBeExcluded = excludedDependencies.some(it => it.name === packageName && it.version === packageVersion);
-      if (shouldBeExcluded) {
-        this.log.info(`Update to ${packageName}:${packageVersion} is excluded`);
-        updatedPackageJson = updatedPackageJson.replace(
-          new RegExp(`("${escapeRegex(packageName)}"\\s*:\\s*)"${escapeRegex(packageVersion)}"`, 'g'),
-          `\$1"${previousPackageJson.dependencies[packageName]}"`);
-      } else {
-        updatesList.push(`\`${packageName}\` updated to \`${ncuState[packageName]}\``);
+      for (let packageName of Object.keys(ncuState)) {
+        const packageVersion = ncuState[packageName];
+        const shouldBeExcluded = excludedDependencies.some(it => it.name === packageName && it.version === packageVersion);
+        if (shouldBeExcluded) {
+          const previousPackageVersion = previousPackageJson.dependencies[packageName]
+            || previousPackageJson.devDependencies[packageName];
+          this.log.info(`Update to ${packageName}:${packageVersion} is excluded, keeping ${previousPackageVersion}`);
+          updatedPackageJson = updatedPackageJson.replace(
+            new RegExp(`("${escapeRegex(packageName)}"\\s*:\\s*)"${escapeRegex(packageVersion)}"`, 'g'),
+            `\$1"${previousPackageVersion}"`);
+        } else {
+          updatesList.push(`\`${packageName}\` updated to \`${ncuState[packageName]}\``);
+        }
       }
-    }
-    this.log.info(`Found updates: ${JSON.stringify(updatesList)}`);
+      this.log.info(`Found updates: ${JSON.stringify(updatesList)}`);
 
-    if (excludedDependencies.length) {
-      this.log.info(`package.json should be updated due to exclusions`);
-      await fs.writeFile(packageFile, updatedPackageJson);
-    }
-
-    const previousPackageLock = !updatesList.length && (await fs.readFile(packageLockFile)).toString();
-
-    this.log.info(`Installing updates`);
-    const npmInstall = execSync('npm install --package-lock-only', {
-      cwd: workingDirectory
-    });
-    this.log.info(`Updates installed with result:\n ${npmInstall.toString()}`);
-
-    this.log.info('Running audit fix');
-    const npmAudit = execSync('npm audit fix --package-lock-only', {
-      cwd: workingDirectory
-    });
-    this.log.info(`Audit finished with result:\n ${npmAudit.toString()}`);
-
-    if (!updatesList.length) {
-      const newPackageLock = (await fs.readFile(packageLockFile)).toString();
-      if (newPackageLock !== previousPackageLock) {
-        this.log.info('New peer dependencies installed');
-        updatesList.push('Updated peer dependencies');
+      if (excludedDependencies.length) {
+        this.log.info(`package.json should be updated due to exclusions`);
+        await fs.writeFile(packageFile, updatedPackageJson);
       }
-    }
 
-    this.log.info('Update check finished');
+      const previousPackageLock = !updatesList.length && (await fs.readFile(packageLockFile)).toString();
 
-    return {
-      title: 'Frontends dependencies',
-      updated: updatesList,
+      this.log.info(`Installing updates`);
+      const {stdout: npmInstallStdout, stderr: npmInstallStderr} = await exec('npm install --package-lock-only', {
+        cwd: workingDirectory
+      });
+      this.log.info(`Updates installed with result:\n ${npmInstallStdout.toString()}\n${npmInstallStderr.toString()}`);
+
+      this.log.info('Running audit fix');
+      const {stdout: npmAuditStdout, stderr: npmAuditStderr} = await exec('npm audit fix --package-lock-only', {
+        cwd: workingDirectory
+      });
+      this.log.info(`Audit finished with result:\n ${npmAuditStdout.toString()}\n${npmAuditStderr.toString()}`);
+
+      if (!updatesList.length) {
+        const newPackageLock = (await fs.readFile(packageLockFile)).toString();
+        if (newPackageLock !== previousPackageLock) {
+          this.log.info('New peer dependencies installed');
+          updatesList.push('Updated peer dependencies');
+        }
+      }
+
+      this.log.info('Update check finished');
+
+      return updatesList.length ? {
+        title: 'Frontends dependencies',
+        updated: updatesList,
+      } : null;
+
+    } catch (e) {
+      this.log.error('Update of frontend dependencies failed');
+      this.log.error(e);
+      return null;
     }
   }
 }
